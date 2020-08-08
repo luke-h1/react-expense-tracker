f6623a4345b11b5dd5be969a152e7812af1b3924
luke-h1 <luke.howsam@yahoo.com>
2020-08-08 15:25:50 +0100

    class > className & for > htmlFor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
     return (
@@ -10,7 +12,9 @@ function App() {
             <Header /> 
             <div className="container">
             <Balance />   
-            <IncomeExpenses /> 
+            <IncomeExpenses />  
+            <TransactionList />  
+            <AddTransaction />
             </div> 
         </div> 
     );
