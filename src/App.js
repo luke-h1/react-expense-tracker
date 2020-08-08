db489d67442b13a5a7ff8a221fe5ed2bcef4d130
luke-h1 <luke.howsam@yahoo.com>
2020-08-08 15:15:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
 
 function App() {
     return (
         <div>
-            <Header />
-        </div>
+            <Header /> 
+            <div className="container">
+            <Balance />   
+            <IncomeExpenses /> 
+            </div> 
+        </div> 
     );
 }
 
